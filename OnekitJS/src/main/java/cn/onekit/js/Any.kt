4d101ed3264592c8fe5a239fb086e51c966ce2f3
15802303637 <>
2020-12-18 00:22:28 +0800

    .

--- OnekitJS/src/main/java/cn/onekit/js/Any.kt
+++ OnekitJS/src/main/java/cn/onekit/js/Any.kt
@@ -1,56 +1,18 @@
 package cn.onekit.js
 
-/*
-fun Any.got(memberName:Any?):Any? {
-    when (this) {
-        is Array<*> -> {
-            val array = this
-            return if (memberName is Number) {
-                if(array.size<=0){
-                    Undefined()
-                }else {
-                    val index = memberName.toInt()
-                    if (index >= 0 || index < array.size) {
-                        array[index]
-                    } else {
-                        Undefined()
-                    }
-                }
-            } else {
-                Undefined()
-            }
+public fun equals(x:Any?,y: Any?): Boolean {
+        if (x == null && y == null) {
+            return true;
         }
-        is Map<*, *> -> {
-            val map = this
-            return if (memberName is String) {
-                val key = memberName.toString()
-                if (map.containsKey(key)) {
-                    map[key]
-                } else {
-                    Undefined()
-                }
-            } else {
-                Undefined()
-            }
+        if (x != null && y == null) {
+            return false;
         }
-        else -> {
-            val name = memberName.toString()
-            for(field in this.javaClass.fields){
-                if(field.name==name){
-                    return field
-                }
-            }
-            for(method in this.javaClass.methods){
-                if(method.name==name){
-                    return method
-                }
-            }
-            return Undefined()
+        if (x == null && y != null) {
+            return false;
         }
+        return  x!!.equals(y)
     }
-}*/
-/////////////////////////////
-fun fullequals(x:Any?,y: Any?) :Boolean {
+public fun fullequals(x:Any?,y: Any?): Boolean {
     if (x == null && y == null) {
         return true;
     }
@@ -61,29 +23,17 @@ fun fullequals(x:Any?,y: Any?) :Boolean {
         return false;
     }
     return x!!.javaClass.name.equals(y!!.javaClass.name)
-            && x.equals(y)
+            && x!!.equals(y)
 }
-fun equals(x:Any?,y: Any?) :Boolean {
-    if (x == null && y == null) {
-        return true;
-    }
-    if (x != null && y == null) {
-        return false;
-    }
-    if (x == null && y != null) {
-        return false;
-    }
-    return x!!.equals(y)
-}
- fun plus(x:Any?,y: Any?) :Any{
+public fun plus(x:Any?,y: Any?) :Any{
     return (x as Number).toDouble()+(y as Number).toDouble()
 }
- fun minus(x:Any?,y: Any?): Any {
+public fun minus(x:Any?,y: Any?): Any {
      return (x as Number).toDouble()-(y as Number).toDouble()
 }
- fun times(x:Any?,y: Any?): Any {
+public fun times(x:Any?,y: Any?): Any {
      return (x as Number).toDouble()*(y as Number).toDouble()
 }
- fun div(x:Any?,y: Any?): Any {
+public fun div(x:Any?,y: Any?): Any {
      return (x as Number).toDouble()/(y as Number).toDouble()
 }
--- app/src/main/java/demo/js/MainActivity.kt
+++ app/src/main/java/demo/js/MainActivity.kt
@@ -11,14 +11,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        class A{
+
+        }
+        class B{
+
+        }
+        val a=A()
+        val b:Any?=null
+        a===b
         ////////////////////
-        val test = object : function { override operator fun invoke(vararg arguments:Any?): Any? {
+        /*val test = object : function { override operator fun invoke(vararg arguments:Any?): Any? {
             val t = arguments[0]
             if(fullequals(t,undefined)){
                 return "Undefined value!"
             }
             return t
-        }}
+        }}*/
+        val test = fun(t:Any?):Any?{
+            if(fullequals(t,undefined)){
+                return "Undefined value!"
+            }
+            return t
+        }
         val x = undefined
         console.log(test(x))
 
@@ -26,5 +41,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
-
 
